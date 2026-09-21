17d9b89cad090bb047f776acd7b5d96063f6089f
mdswaley <mdswaley075@gmail.com>
2026-09-21 12:19:31 +0530

    create DB connection

---
+++ src/main/java/com/kodewala/Util/DBConnection.java
@@ -0,0 +1,15 @@
+package com.kodewala.Util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/vehicle_system";
+    private static final String USER = "root";
+    private static final String PASSWORD = "2003";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
