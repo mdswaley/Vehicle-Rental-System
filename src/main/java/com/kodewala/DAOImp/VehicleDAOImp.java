2de1f252016f5f9c5813b0f743e542db6031f629
mdswaley <mdswaley075@gmail.com>
2026-09-22 08:42:10 +0530

    woking on vehicleDAOImp class

--- src/main/java/com/kodewala/DAOImp/VehicleDAOImp.java
+++ src/main/java/com/kodewala/DAOImp/VehicleDAOImp.java
@@ -1,4 +1,51 @@
 package com.kodewala.DAOImp;
 
-public class VehicleDAOImp {
+import com.kodewala.DAO.VehicleDAO;
+import com.kodewala.Model.Constant.VehicleStatus;
+import com.kodewala.Model.Vehicle;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public class VehicleDAOImp implements VehicleDAO {
+
+    private final Connection connection;
+
+    VehicleDAOImp(Connection connection){
+        this.connection = connection;
+    }
+
+    @Override
+    public void addVehicle(Vehicle vehicle) throws SQLException {
+        String sql = """
+                INSERT INTO vehicle (vehicleNumber, brand, model, vehicleType, pricePerDay, status) VALUES (?, ?, ?, ?, ?, ?)
+                """;
+
+    }
+
+    @Override
+    public Vehicle getVehicleById(int id) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List<Vehicle> getAllVehicles() throws SQLException {
+        return List.of();
+    }
+
+    @Override
+    public List<Vehicle> getAvailableVehicles() throws SQLException {
+        return List.of();
+    }
+
+    @Override
+    public void updateVehicle(Vehicle vehicle) throws SQLException {
+
+    }
+
+    @Override
+    public void deleteVehicle(int id) throws SQLException {
+
+    }
 }
